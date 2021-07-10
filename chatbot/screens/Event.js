0dac3b17b27f6a8865a75a2cbe7f1054747176ca
unknown <mohammedfirdaus1404@gmail.com>
2021-07-11 00:16:40 +0700

    Screen untuk Organisasi sama Acara

--- chatbot/screens/Event.js
+++ chatbot/screens/Event.js
@@ -3,40 +3,83 @@ import { View, Text, Image, StyleSheet, Dimensions, SafeAreaView } from 'react-n
 import Icon from 'react-native-vector-icons/AntDesign';
 import { SearchBar } from 'react-native-elements';
 import { ScrollView } from 'react-native';
+import Header from './header'
 
 const win = Dimensions.get('window');
 const caro = win / 5;
 const logo_size = 12;
 const ratio = win.width / 2419;
 
-const book = [
-    require('../assets/image1.png'),
-    require('../assets/image2.png'),
-    require('../assets/image3.png'),
-    require('../assets/image4.png'),
-    require('../assets/image5.png'),
-    require('../assets/image6.png'),
-    require('../assets/image7.png'),
-    require('../assets/image8.png')
+const semuaAcara = [
+    {
+        id: '1',
+        tipe: "Batch 2020",
+        nama: "Caroons Party"
+    },
+    {
+        id: '2',
+        tipe: "Committee",
+        nama: "Innofair 2021"
+    },
+    {
+        id: '3',
+        tipe: "Committee",
+        nama: "PGU 2021"
+    }
+
 ]
 
 export default class Event extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedLanguage: '',
+            All_Courses: [
+                {
+                    id: '1',
+                    orga: 'Student Board',
+                    acaranya: 'Student Board Member',
+                    image: require('../assets/sb.png'),
+                },
+                {
+                    id: '2',
+                    orga: 'SISO',
+                    acaranya: 'Ketua dan Wakil Ketua SISO',
+                    image: require('../assets/siso.png'),
+                },
+                {
+                    id: '3',
+                    orga: 'Student Board',
+                    acaranya: 'Lodestar 2021',
+                    image: require('../assets/sb.png'),
+                },
+                {
+                    id: '4',
+                    orga: 'Student Board',
+                    acaranya: 'Social Week 2021',
+                    image: require('../assets/sb.png'),
+                },
+                {
+                    id: '5',
+                    orga: 'Eureca',
+                    acaranya: 'Eureca',
+                    image: require('../assets/eureca.png'),
+                },
+                {
+                    id: '6',
+                    orga: 'Carya',
+                    acaranya: 'Carya',
+                    image: require('../assets/carya.png'),
+                },
+            ],
         };
     }
 
     render() {
         return (
-            <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
-                <View style={styles.header}>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Icon name="arrowleft" size={30} color="white" style={{ marginRight: 5 }} onPress={() => this.props.navigation.navigate('Home')} />
-                    </View>
-                    <Icon name="mail" size={30} color="white" style={{ marginRight: 5 }} />
-                </View>
+            <View style={{ flex: 1, backgroundColor: 'white' }}>
+                <Header
+                    navigationpage={() => this.props.navigation.navigate('Home')}
+                />
 
                 <View style={{ flexDirection: 'row' }}>
                     <Icon name="team" size={50} color="5F5C5C" style={{ margin: 15 }} />
@@ -45,7 +88,68 @@ export default class Event extends React.Component {
                         <Text style={styles.h2}>See what events and organizations is comming</Text>
                     </View>
                 </View>
-            </ScrollView>
+
+                {/* Line */}
+                <View
+                    style={{
+                        borderBottomColor: '#C7C7C7',
+                        borderBottomWidth: 3,
+                    }}
+                />
+                <View>
+                    <Text style={{ padding: 12, fontFamily: 'Roboto-Bold', fontSize: 17, marginBottom: 10 }}>Recently Accessed Course</Text>
+                </View>
+                <ScrollView pagingEnabled horizontal>
+                    {
+                        semuaAcara.map((acara, index) => (
+                            <View key={index} style={{ marginRight: 15, borderColor: 'grey', borderWidth: 1, height: "25%", width: win.width * 7.05 / 10, borderBottomLeftRadius: 15, borderBottomRightRadius: 15, elevation: 4, backgroundColor: 'white'}}>
+                                <View>
+                                    <Image
+                                        source={require('../assets/accessed-course.png')}
+                                        style={{ width: win.width * 7 / 10, height: win.width * 300 / 1231 * 7 / 10 }}
+                                    />
+                                    <View style={{ padding: 10 }}>
+                                        <Text style={{ fontFamily: 'Roboto-Bold', fontSize: 12 }}>{acara.tipe}</Text>
+                                        <Text style={{ fontFamily: 'Roboto-Regular', fontSize: 11 }}>{acara.nama}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        ))
+                    }
+                </ScrollView>
+
+                {/* Ini Buat Garis */}
+                <View style={{ width: win.width, height: 6, backgroundColor: '#C7C7C7', marginTop: -350 }} />
+
+                {/* Grid Organisasi */}
+                <View style={{ padding: 12, flex: 1, paddingBottom: 0 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 15, alignItems: 'center' }}>
+                        <Text style={{ fontFamily: "Roboto-Bold", fontSize: 15 }}>Open Recruitment</Text>
+                        <View style={{ flexDirection: 'row', marginRight: 20 }}>
+                            <Text style={{ fontFamily: 'Roboto-Bold' }}>Session </Text>
+                            <Text style={{ fontFamily: 'Roboto-Regular', borderWidth: 0.5, borderColor: 'black', paddingHorizontal: 6 }}>Even, 2020</Text>
+                        </View>
+                    </View>
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                            {this.state.All_Courses.map((item, index) => (
+                                <View key={index} style={{ borderColor: 'grey', borderWidth: 1, width: win.width * 4.56 / 10, marginBottom: 12, borderBottomLeftRadius: 15, borderBottomRightRadius: 15 }}>
+                                    <View>
+                                        <Image
+                                            source={item.image}
+                                            style={{ height: 70, width: 70, alignSelf: "center", marginTop: 5}}
+                                        />
+                                        <View style={{ padding: 8, paddingTop: 4 }}>
+                                            <Text style={{ fontFamily: 'Roboto-Bold', fontSize: 12 }}>{item.orga}</Text>
+                                            <Text style={{ fontFamily: 'Roboto-Regular', fontSize: 11 }}>{item.acaranya}</Text>
+                                        </View>
+                                    </View>
+                                </View>
+                            ))}
+                        </View>
+                    </ScrollView>
+                </View>
+            </View>
         )
     }
 }
